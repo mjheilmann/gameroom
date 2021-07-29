14da6bb257112646a57501acdd4559ae49118750
Michael Heilmann <michael.j.heilmann@gmail.com>
2021-07-28 22:23:56 -0400

    simplify template, add win function

--- lib/gameroom_web/live/tictac/tictac.ex
+++ lib/gameroom_web/live/tictac/tictac.ex
@@ -1,23 +1,19 @@
 defmodule GameroomWeb.TicTac do
   use Phoenix.LiveComponent
 
-  @characters [:x, :o]
+  @pieces [:x, :o]
   @initial_board [nil, nil, nil, nil, nil, nil, nil, nil, nil]
 
   @impl true
   def mount(socket) do
-    {:ok, assign(socket, try_me_counter: 0, board: @initial_board)}
+    {:ok, assign(socket, my_turn: false, board: @initial_board)}
   end
 
   @impl true
   def render(assigns) do
     ~L"""
     <div class="" id="<%= @id %>">
-      <div>
-        <h1>tic tac my honkies</h1>
-        <button phx-click="inc_try_me" phx-target="<%= @myself %>">Try Me</button> <span>counter: <%= @try_me_counter %></span>
-      </div>
-
+    <h4>Tic Tac Toe</h4>
       <section class="column">
         <%= for row <- @board |> Enum.with_index |> Enum.chunk_every(3) do %>
           <article class="row">
@@ -27,6 +23,11 @@ defmodule GameroomWeb.TicTac do
           </article>
         <% end %>
       </section>
+          <%= if @my_turn do %>
+    <p>It is your turn</p>
+    <% else %>
+    <p>It is NOT your turn</p>
+    <% end %>
     </div>
     """
   end
@@ -54,6 +55,21 @@ defmodule GameroomWeb.TicTac do
           socket.assigns.board
       end
 
+    if checkwin(board) do
+      IO.puts("winner")
+    end
+
     {:noreply, assign(socket, :board, board)}
   end
+
+  defp checkwin([x, x, x, _, _, _, _, _, _]) when x in @pieces, do: true
+  defp checkwin([_, _, _, x, x, x, _, _, _]) when x in @pieces, do: true
+  defp checkwin([_, _, _, _, _, _, x, x, x]) when x in @pieces, do: true
+  defp checkwin([x, _, _, x, _, _, x, _, _]) when x in @pieces, do: true
+  defp checkwin([_, x, _, _, x, _, _, x, _]) when x in @pieces, do: true
+  defp checkwin([_, _, x, _, _, x, _, _, x]) when x in @pieces, do: true
+  defp checkwin([x, _, _, _, x, _, _, _, x]) when x in @pieces, do: true
+  defp checkwin([_, _, x, _, x, _, x, _, _]) when x in @pieces, do: true
+
+  defp checkwin(_), do: false
 end
